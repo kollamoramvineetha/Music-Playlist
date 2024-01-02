380a424afd88d2e0c825d9b40a843e9554d19b69
Vineetha Kollamoram <90500447+kollamoramvineetha@users.noreply.github.com>
2024-01-02 10:30:49 +0530

    Update index.js

--- src/components/Object/index.js
+++ src/components/Object/index.js
@@ -1,2 +1,15 @@
-const Object = () => <h1>Hi</h1>
+const Object = props => {
+  const {details} = props
+  const {imageUrl, name, genre, duration} = details
+  return (
+    <div>
+      <li>
+        <img src={imageUrl} alt="track" />
+        <p>{name}</p>
+        <p>{genre}</p>
+        <p>{duration}</p>
+      </li>
+    </div>
+  )
+}
 export default Object
